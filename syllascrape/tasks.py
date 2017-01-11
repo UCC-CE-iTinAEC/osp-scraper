ae00f13022e709c0e023044a577dca61bd11a259
David McClure <davidwilliammcclure@gmail.com>
2017-01-10 23:09:16 -0800

    Block in a simplified crawl() task, designed for use under RQ.

--- syllascrape/tasks.py
+++ syllascrape/tasks.py
@@ -1,34 +1,19 @@
+
+
 from __future__ import absolute_import
 
-from .celery import app
-from .spiders import Spider
-
-from scrapy.crawler import CrawlerRunner
-from twisted.internet import reactor
-from billiard import Process
+from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
-class CrawlerSubprocess(Process):
-    """Crawling via subprocess under celery
+from .spiders import Spider
 
-    Based on http://stackoverflow.com/a/22202877 and
-    http://doc.scrapy.org/en/latest/topics/practices.html#run-scrapy-from-a-script
+
+def crawl(spider, *args, **kwargs):
+    """Run a spider.
+
+    Args:
+        spider (str): The Scrapy `name` of the spider.
     """
-
-    def __init__(self, *args, **kwargs):
-        super().__init__()
-        self.runner = CrawlerRunner(get_project_settings())
-        self.args = args
-        self.kwargs = kwargs
-
-    def run(self):
-        d = self.runner.crawl(Spider, *self.args, **self.kwargs)
-        d.addBoth(lambda _: reactor.stop())
-        reactor.run()
-
-
-@app.task
-def crawl(*args, **kwargs):
-    proc = CrawlerSubprocess(*args, **kwargs)
-    proc.start()
+    proc = CrawlerProcess(get_project_settings())
+    proc.crawl(spider)
     proc.join()
