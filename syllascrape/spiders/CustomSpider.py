59064fd42c86f4642d16395e3fc2bc34aed68f86
Harrison Gregg <hcg28@cornell.edu>
2017-01-23 11:25:19 -0500

    Bug fixes and refactoring with CustomSpider base class

---
+++ syllascrape/spiders/CustomSpider.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import re
+import uuid
+
+from scrapy.spiders import Spider
+
+from ..filterware import Filter
+
+class CustomSpider(Spider):
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        spider = super().from_crawler(crawler, *args, **kwargs)
+        spider.run_id = str(uuid.uuid1())
+        spider.allowed_file_types = {'pdf', 'doc', 'docx'}
+        spider.filters = [Filter.compile('allow')]
+        return spider
+
+    def get_parameters(self):
+        """return dict of parameters for current spider run"""
+        # default values should match various middlewares
+        return {
+            'filters': [f.asdict() for f in getattr(self, 'filters', [])],
+            'start_urls': getattr(self, 'start_urls', []),
+            'allowed_file_types': list(getattr(self, 'allowed_file_types', set()))
+        }
+
+    def clean_whitespace(self, s):
+        return re.sub(r"\s+", " ", s).strip()
--- syllascrape/spiders/asu.py
+++ syllascrape/spiders/asu.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import scrapy
-from ..items import PageItem 
 
-class ASUSpider(scrapy.spiders.Spider):
+from ..items import PageItem 
+from ..spiders.CustomSpider import CustomSpider
+
+class ASUSpider(CustomSpider):
     name = "asu"
     allowed_domains = ["asu.edu"]
 
@@ -14,16 +16,21 @@ class ASUSpider(scrapy.spiders.Spider):
         start_url = "https://webapp4.asu.edu/catalog/"
 
         def get_terms_codes(response):
-            terms.extend(response.css("#term option::attr(value)").extract())
+            for option in response.css("#term option"):
+                code = option.css("option::attr(value)").extract_first()
+                name = option.css("option::text").extract_first()
+                terms.append((code, name))
+
             return scrapy.Request("https://webapp4.asu.edu/catalog/Subjects.html", callback=get_subject_codes)
 
         def get_subject_codes(response):
-            subjects.extend(response.css(".subject::text").extract())
-            return start_form_requests()
+            for subject in response.css("#subjectDivs .row"):
+                code = subject.css(".subject::text").extract_first()
+                name = subject.css(".subjectTitle::text").extract_first()
+                subjects.append((code, name))
 
-        def start_form_requests():
-            for term_code in terms:
-                for subject_code in subjects:
+            for term_code, term_name in terms[:3]:
+                for subject_code, subject_name in subjects[:3]:
                     yield scrapy.FormRequest(
                         "https://webapp4.asu.edu/catalog/classlist",
                         formdata={
@@ -39,32 +46,30 @@ class ASUSpider(scrapy.spiders.Spider):
                             "depth": 1,
                             "hops_from_seed": 1,
                             "source_url": start_url,
-                            "source_anchor": "",
+                            "source_anchor": subject_name + " " + term_name,
                         },
-                        callback=self.parse
+                        callback=self.parse,
+                        dont_filter=True
                     )
 
         yield scrapy.Request(start_url, callback=get_terms_codes, dont_filter=True)
 
-
-
     def parse(self, response):
-
         file_urls = []
 
-        syllabi_tags = response.css('a[title="Syllabus"]').extract()
-        for tag in syllabi_tags:
-            url = tag.css('base::attr(href)')
-            anchor = tag.css('base::text')
+        for tag in response.css('a[title="Syllabus"]'):
+            url = tag.css('a::attr(href)').extract_first()
+            if url:
+                anchor = tag.css('a::text').extract_first()
 
-            meta = {
-                'source_url': response.url,
-                'source_anchor': anchor,
-                'depth': response.meta['depth'] + 1,
-                'hops_from_seed': response.meta['hops_from_seed'] + 1,
-            }
+                meta = {
+                    'source_url': response.url,
+                    'source_anchor': anchor,
+                    'depth': response.meta['depth'] + 1,
+                    'hops_from_seed': response.meta['hops_from_seed'] + 1,
+                }
 
-            file_urls.append((url, meta))
+                file_urls.append((url, meta))
 
         yield PageItem(
             url=response.url,
