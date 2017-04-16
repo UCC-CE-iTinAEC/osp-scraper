3238234dd49b8cc205c69df25e754fa9474c62f7
Harrison Gregg <hcg28@cornell.edu>
2017-04-16 14:57:44 -0400

    Finish Marylhurst scraper

--- osp_scraper/spiders/marylhurst.py
+++ osp_scraper/spiders/marylhurst.py
@@ -10,22 +10,56 @@ class MarylhurstSpider(CustomSpider):
     start_urls = ["http://my.marylhurst.edu/soc/default.aspx"]
 
     def parse(self, response):
-        terms = response.css("#termDropdown option")
+        terms = response.css("#termDropdown option::attr(value)").extract()
         for term in terms:
-            value = term.css("::attr(value)").extract_first()
-            anchor = term.css("::text").extract_first()
             yield scrapy.FormRequest.from_response(
                 response,
                 method="POST",
                 formdata={
-                    'termDropdown': value,
-                    'filterDropdown': "ALL+DEPARTMENTS"
+                    'termDropdown': term,
+                    'filterDropdown': "ALL DEPARTMENTS"
                 },
                 callback=self.parse_for_courses
             )
 
     def parse_for_courses(self, response):
-        pass
+        a_tags = response.css("#scheduleTable tr a")
+        for a_tag in a_tags:
+            # onclick is of the form:
+            # return showCrsDescription('<code>', '<term>');
+            code, term = a_tag.css("::attr(onclick)").re("'(.*?)'")
+            course_name = a_tag.css("::text").extract_first()
+            yield scrapy.FormRequest(
+                "http://my.marylhurst.edu/soc/Webservice.asmx/GetCourseDescription",
+                method="POST",
+                formdata={
+                    'CourseCode': code + "_" + term
+                },
+                meta={
+                    'source_anchor': code + " " + course_name
+                },
+                callback=self.parse_for_syllabus_link
+            )
+
+    def parse_for_syllabus_link(self, response):
+        # Response is xml with escaped html within.
+        url = response.css("*").re_first("href='(.*?)'")
+        yield scrapy.Request(
+            url,
+            meta={
+                'depth': 2,
+                'hops_from_seed': 2,
+                'source_url': response.url,
+                'source_anchor': response.meta["source_anchor"]
+            },
+            callback=self.parse_for_files
+        )
 
     def extract_links(self, response):
-        pass
+        # Most courses have an HTML syllabus, but a few link to files instead.
+        for a_tag in response.css("a"):
+            url = a_tag.css("::attr(href)").extract_first()
+            a_text = a_tag.css("::text").extract_first()
+            if "syllabus" in a_text.lower():
+                anchor = response.meta['source_anchor'] + " " + a_text
+                yield (url, anchor)
