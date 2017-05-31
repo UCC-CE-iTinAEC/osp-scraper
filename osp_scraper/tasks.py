71651f3028d8ca0142875b97b19ebf145e79292f
David McClure <davidwilliammcclure@gmail.com>
2017-05-31 16:10:01 -0700

    Bump task timeout to 1 week.

--- osp_scraper/tasks.py
+++ osp_scraper/tasks.py
@@ -92,8 +92,8 @@ def make_params(seed_urls):
     return d
 
 
-# Run crawls for 1 day max.
-@job('default', connection=redis_conn, timeout=86400)
+# Run crawls for 1 week max.
+@job('default', connection=redis_conn, timeout=60*60*24*7)
 def crawl(spider, *args, **kwargs):
     """Run a spider.
 
