e863249e1870b930b0ad78478cba6d77515573f9
David McClure <davidwilliammcclure@gmail.com>
2017-06-01 16:07:50 -0700

    Final ScraperWorker#request_stop().

--- osp_scraper/workers.py
+++ osp_scraper/workers.py
@@ -6,4 +6,15 @@ from rq import Worker
 class ScraperWorker(Worker):
 
     def request_stop(self, *args, **kwargs):
+        """When SIGINT is sent to the worker (eg, if the Supervisor process
+        group is restarted), immediately fail the running job and stop the
+        worker. This avoids a scenario in which the worker gets shut down but
+        not unregistered in Redis, causing it to get "marooned" in the admin.
+        """
+        job = self.get_current_job()
+
+        if job:
+            self.handle_job_failure(job)
+            self.failed_queue.quarantine(job, 'Worker shutdown.')
+
         self.request_force_stop(*args, **kwargs)
